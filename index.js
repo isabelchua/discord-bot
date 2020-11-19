105b9888a639efeb45afc93e9fe395fe12a5490e
isabelchua <isabelchua86@gmail.com>
2020-11-18 20:38:16 -0800

    switch command bot

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const { token } = require("./config.json");
 
-const token = "Nzc4NzQ3OTA4MjQxMDk2NzI2.X7WfgQ.yxJIsHmRrgavDJUqkqrfY964LZI";
+// const token = process.env.TOKEN;
+
+const PREFIX = "!";
+
+var version = "1.0.1";
 
 bot.on("ready", () => {
 	console.log("Bot is online");
 });
 
-bot.on("message", msg => {
-	if (msg.content === "hello") {
-		msg.reply("Hello friend!");
+bot.on("message", message => {
+	let args = message.content.substring(PREFIX.length).split(" ");
+
+	switch (args[0]) {
+		case "ping":
+			message.channel.send("pong!");
+			break;
+		case "website":
+			message.channel.send("youtube.com");
+			break;
+		case "info":
+			if (args[1] === "version") {
+				message.channel.send(version);
+			} else {
+				message.channel.send("Invalid Command");
+			}
 	}
+	// if (msg.content === "hello") {
+	// 	msg.reply("Hello friend!");
+	// }
 });
 
 bot.login(token);
